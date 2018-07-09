63b933e56ad27bcb2167d44729190ba3fce98a59
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-09 14:34:46 -0700

    Initial Commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by César Francisco Barraza on 7/9/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstagramClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramClone
+//
+//  Created by César Francisco Barraza on 7/9/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
